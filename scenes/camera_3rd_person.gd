3a1ab2ee957b857240bda3e5b1e47910eb094062
pohy <0ronon0@gmail.com>
2023-07-23 21:17:46 +0200

    Make the camera controls feel gooooood! :)

--- scenes/camera_3rd_person.gd
+++ scenes/camera_3rd_person.gd
@@ -4,10 +4,12 @@ extends Node3D
 
 @export var offset: Vector3 = Vector3.ZERO
 @export var target: Node3D
-@export var sensitivity: float = 0.1
+@export var sensitivity: Vector2 = Vector2(0.1, 0.1)
 @export var speed: float = 1.0
+@export var max_camera_rotation: Vector2 = Vector2(-30, 60)
 
 @onready var _look_target: Node3D = $LookTarget
+@onready var _look_direction: Node3D = $LookTarget/Direction
 @onready var _camera: Camera3D = $Camera3D
 @onready var _mouse: Mouse = $Mouse
 
@@ -20,7 +22,8 @@ func _ready():
 
 
 func _process(delta):
-	position = target.global_transform.origin
+	if target:
+		position = target.global_transform.origin
 
 	if Engine.is_editor_hint():
 		return
@@ -38,18 +41,27 @@ func _get_configuration_warnings():
 func rotate_towards_rotation_target(delta: float):
 	if not target:
 		return
-	if not _mouse.right:
-		_target_y_rot = 0.
-		return
 
-	_target_y_rot += -_mouse.delta.x * sensitivity
+	if _mouse.right:
+		_look_target.rotate_object_local(Vector3.UP, -_mouse.delta.x * sensitivity.x)
+		_look_direction.translate(Vector3.UP * -_mouse.delta.y * sensitivity.y)
 
-	_rotation_target = Basis.from_euler(Vector3.UP * _target_y_rot, EULER_ORDER_XYZ)
+	transform.basis = (
+		transform.basis.slerp(_look_target.transform.basis, speed * delta).orthonormalized()
+	)
 
-	var begin_rotation_y = transform.basis.get_euler(EULER_ORDER_XYZ).y
-	transform.basis = transform.basis.slerp(_rotation_target, speed * delta).orthonormalized()
-
-	_target_y_rot -= begin_rotation_y - transform.basis.get_euler(EULER_ORDER_XYZ).y
+	var next_camera_basis = (
+		_camera
+		. transform
+		. basis
+		. slerp(
+			_camera.transform.looking_at(_look_direction.position, Vector3.UP).basis, speed * delta
+		)
+		. orthonormalized()
+	)
+	var camera_rotation = next_camera_basis.get_euler() * 180 / PI
+	if camera_rotation.x > max_camera_rotation.x and camera_rotation.x < max_camera_rotation.y:
+		_camera.transform.basis = next_camera_basis
 
 
 func look_towards_look_target():
--- scenes/position_target.gd
+++ scenes/position_target.gd
@@ -1,5 +1,5 @@
 @tool
-extends StaticBody3D
+extends Area3D
 
 @export var radius_multiplier: float = 1.
 
@@ -12,10 +12,14 @@ var _camera: Camera3D
 
 
 func _ready():
+	_cylinder_shape_3d = _collision_shape_3d.shape as CylinderShape3D
 	_camera = get_viewport().get_camera_3d()
 
 
 func _process(delta):
+	if not _collision_shape_3d or not _collision_shape_3d.shape:
+		return
+
 	_cylinder_shape_3d = _collision_shape_3d.shape as CylinderShape3D
 	_csg_cylinder.radius = _cylinder_shape_3d.radius * radius_multiplier
 	_csg_cylinder.height = _cylinder_shape_3d.height
@@ -35,38 +39,43 @@ func _get_configuration_warnings():
 	return warnings
 
 
-func _on_input_event(
-	_camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int
-):
-	print(
-		"on_input_event - event: ",
-		event,
-		" position: ",
-		position,
-		" normal: ",
-		normal,
-		" shape_idx: ",
-		shape_idx
-	)
-	pass  # Replace with function body.
-
-
-func move_towards_mouse_cursor(event):
-	var origin = _camera.project_ray_origin(event.position)
-	var ray_target = origin + _camera.project_ray_normal(event.position) * 1000
-	var ray = (
-		PhysicsRayQueryParameters3D
-		. create(
-			origin,
-			ray_target,
-		)
-	)
-
-	var space_state = get_world_3d().direct_space_state
-	var result = space_state.intersect_ray(ray)
+func move_towards_mouse_cursor(event: InputEventMouseMotion):
+	var result = get_target_position()
 	if not result or not result.collider:
 		return
 
 	var target_position = result.position
 	target_position.y = position.y
 	position = target_position
+
+
+func get_target_position() -> Dictionary:
+	var origin = _camera.project_ray_origin(_mouse.pos)
+	var ray_target = origin + _camera.project_ray_normal(_mouse.pos) * 1000
+	var query = PhysicsRayQueryParameters3D.create(origin, ray_target, 0b01, [self])
+
+	var space_state = get_world_3d().direct_space_state
+	return space_state.intersect_ray(query)
+
+
+func _on_body_entered(body: StaticBody3D):
+	if not body.is_in_group("cover"):
+		return
+	var query = PhysicsShapeQueryParameters3D.new()
+	query.transform = transform
+	query.shape = _cylinder_shape_3d
+	query.shape_rid = _cylinder_shape_3d.get_rid()
+	query.exclude = [self]
+	query.collide_with_areas = true
+	query.collide_with_bodies = false
+	query.collision_mask = collision_mask
+	var space_state = get_world_3d().direct_space_state
+	var result = space_state.intersect_shape(query)
+	print_debug("Will snap to cover", result)
+	if not result or not result.collider:
+		return
+	pass  # Replace with function body.
+
+
+func _on_body_exited(body):
+	pass  # Replace with function body.
