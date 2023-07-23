0ee814efe62fe7e7a74a20ccc9540449d62af3f1
pohy <0ronon0@gmail.com>
2023-07-23 22:04:50 +0200

    Work on position target snapping into cover

--- scenes/position_target.gd
+++ scenes/position_target.gd
@@ -2,13 +2,16 @@
 extends Area3D
 
 @export var radius_multiplier: float = 1.
+@export var speed: float = 1.
 
 @onready var _collision_shape_3d: CollisionShape3D = $CollisionShape3D
 @onready var _csg_cylinder: CSGCylinder3D = $CSGCylinder3D
 @onready var _mouse: Mouse = $Mouse
+@onready var _debug_sphere: CSGSphere3D = $DebugSphere
 
 var _cylinder_shape_3d: CylinderShape3D
 var _camera: Camera3D
+var _snap_target
 
 
 func _ready():
@@ -17,17 +20,22 @@ func _ready():
 
 
 func _process(delta):
-	if not _collision_shape_3d or not _collision_shape_3d.shape:
+	sync_csg_to_collision_shape()
+
+	if Engine.is_editor_hint():
 		return
 
-	_cylinder_shape_3d = _collision_shape_3d.shape as CylinderShape3D
-	_csg_cylinder.radius = _cylinder_shape_3d.radius * radius_multiplier
-	_csg_cylinder.height = _cylinder_shape_3d.height
+	move_towards_mouse_cursor()
+	try_to_snap_to_cover()
 
 
-func _input(event):
-	if event is InputEventMouseMotion and not _mouse.right:
-		move_towards_mouse_cursor(event)
+func _on_body_entered(body):
+	var dir = (body.global_transform.origin - global_transform.origin).normalized()
+	_snap_target = find_snap_target(dir)
+
+
+func _on_body_exited(body):
+	_snap_target = null
 
 
 func _get_configuration_warnings():
@@ -39,12 +47,20 @@ func _get_configuration_warnings():
 	return warnings
 
 
-func move_towards_mouse_cursor(event: InputEventMouseMotion):
+func sync_csg_to_collision_shape():
+	if not _collision_shape_3d or not _collision_shape_3d.shape:
+		return
+
+	_csg_cylinder.radius = _cylinder_shape_3d.radius * radius_multiplier
+	_csg_cylinder.height = _cylinder_shape_3d.height
+
+
+func move_towards_mouse_cursor():
 	var result = get_target_position()
 	if not result or not result.collider:
 		return
 
-	var target_position = result.position
+	var target_position = position.slerp(result.position, get_process_delta_time() * speed)
 	target_position.y = position.y
 	position = target_position
 
@@ -58,24 +74,18 @@ func get_target_position() -> Dictionary:
 	return space_state.intersect_ray(query)
 
 
-func _on_body_entered(body: StaticBody3D):
-	if not body.is_in_group("cover"):
+func try_to_snap_to_cover():
+	if _snap_target == null:
 		return
-	var query = PhysicsShapeQueryParameters3D.new()
-	query.transform = transform
-	query.shape = _cylinder_shape_3d
-	query.shape_rid = _cylinder_shape_3d.get_rid()
-	query.exclude = [self]
-	query.collide_with_areas = true
-	query.collide_with_bodies = false
-	query.collision_mask = collision_mask
+
+	var result = _snap_target as Dictionary
+	_debug_sphere.position = result.position + result.normal * _cylinder_shape_3d.radius
+	_debug_sphere.look_at(result.position + result.normal, Vector3.UP)
+
+
+func find_snap_target(dir: Vector3) -> Dictionary:
+	var query = PhysicsRayQueryParameters3D.create(
+		position, position + dir * 1000, collision_mask, [self]
+	)
 	var space_state = get_world_3d().direct_space_state
-	var result = space_state.intersect_shape(query)
-	print_debug("Will snap to cover", result)
-	if not result or not result.collider:
-		return
-	pass  # Replace with function body.
-
-
-func _on_body_exited(body):
-	pass  # Replace with function body.
+	return space_state.intersect_ray(query)
