5aaa3cb3e4455ef5b6d1d2109d9c8dc4926bea5b
pohy <0ronon0@gmail.com>
2023-07-24 13:40:24 +0200

    Add unit highlight component

--- scenes/movement_controls.gd
+++ scenes/movement_controls.gd
@@ -3,6 +3,8 @@ class_name MovementControls
 extends Node
 
 @export var speed: float = 1.0
+@export var acceleration: float = 1.0
+@export var friction: float = 1.0
 @export var camera: Camera3rdPerson
 
 @onready var _input: Inpt = $Input
@@ -15,8 +17,6 @@ func _get_configuration_warnings():
 	var warnings = []
 	if _character == null:
 		warnings.append("CharacterBody3D parent is null.")
-	# if not camera:
-	# 	warnings.append("No camera assigned to the character")
 	return warnings
 
 
@@ -28,8 +28,9 @@ func _process(delta):
 	if Engine.is_editor_hint():
 		return
 
-	var next_dir = Vector3.ZERO
 	var input_dir = Vector3(_input.dir.x, 0, _input.dir.y)
+
+	var next_dir = Vector3.ZERO
 	if not camera:
 		next_dir = input_dir
 	else:
@@ -37,8 +38,12 @@ func _process(delta):
 		if _mouse.left and _mouse.right and input_dir.length() < 0.001:
 			next_dir = -Vector3.FORWARD
 		next_dir = next_dir.rotated(Vector3.UP, camera.look_target.rotation.y)
+	next_dir = next_dir.normalized()
 
-	_character.velocity = next_dir * speed
+	var target_velocity = next_dir * speed
+	var current_acceleration = acceleration if next_dir.length() > 0 else friction
+
+	_character.velocity = _character.velocity.slerp(target_velocity, delta * current_acceleration)
 
 
 func _physics_process(delta):
